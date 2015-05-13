50ed810bcc4b0525e2e333dafb2d65902adc89a2
herbertz0 <howherbertzo@gmail.com>
2015-05-13 11:57:45 +0200

    Compatibility

--- config.h
+++ config.h
@@ -1,14 +1,12 @@
 #ifndef CONFIG_H
     #define CONFIG_H
-    #define DEBUG 1 // If enable, debugging routines will be compiled
+    #define DEBUG // If enable, debugging routines will be compiled
 
 	// The following preprocessor code selects the architecture
 	#ifdef __AVR__
         #warning AVR plateform selected
         #define BAUD_RATE 0x19      // 38400
 		#define F_CPU	16000000UL  // CPU clock frequency
-		#include <avr/io.h>
-		#define EXIT_FAILURE -1
     #else
         #warning Windows plateform selected
         #include <inttypes.h>
--- main.c
+++ main.c
@@ -1,32 +1,30 @@
 #include "config.h"
-#include "src/include/poly.h"
+#include "../lib_data/data.h"
+#include "src/poly.h"
 
 extern Poly* syndrome;
 
 int main(int argc, char *argv[])
 {
     Poly* syndrome = make_syndrome();
-    uint16_t i = 0;
-    for(i = 0; i<N; i++)
-        data_show(syndrome[i]);
-//    Poly message = data_generate(K);
-//    data_set(0, 1, message);
-//    data_set(1, 1, message);
-//    data_set(2, 1, message);
-//    data_show(message);
-//
-//    Poly coded_word = poly_encode(message);
-//    data_set(0,1,coded_word); // Add an error
-//    data_show(coded_word);
-//
-//    Poly decoded = poly_decode(coded_word);
-//    data_show(decoded);
-//
-//    data_free(decoded);
-//    data_free(coded_word);
-//    data_free(message);
-    //(x+1)(x^2+x+1) = X^3+X^2+X +X^2+X+1 = X^3 +1
 
+    Poly message = data_generate(K);
+    data_set(0, 1, message);
+    data_set(1, 1, message);
+    data_set(2, 1, message);
+    data_show(message);
+
+    Poly coded_word = poly_encode(message);
+    data_set(0,1,coded_word); // Add an error
+    data_show(coded_word);
+
+    Poly decoded = poly_decode(coded_word);
+    data_show(decoded);
+
+    data_free(decoded);
+    data_free(coded_word);
+    data_free(message);
+    //(x+1)(x^2+x+1) = X^3+X^2+X +X^2+X+1 = X^3 +1
 
     return EXIT_SUCCESS;
 }
