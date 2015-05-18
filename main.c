6ec42ed760faaaccfa8fd0f217be3fbcb027ebad
herbertz0 <howherbertzo@gmail.com>
2015-05-18 19:22:28 +0200

    Stable release

--- main.c
+++ main.c
@@ -9,39 +9,37 @@ void test();
 
 int main(int argc, char *argv[])
 {
-    while(1){
     generator = generator_to_poly();
     make_syndrome();
-//
-//    printf("Message: ");
-//    Poly message = data_generate(K);
-//    data_set(2, 1, message);
-//    data_show(message);
-//
-//    printf("Coded word: ");
-//    Poly coded_word = poly_encode(message);
-//    data_show(coded_word);
-//    data_set(6,1,coded_word); // Add an error
-//    printf("Modified codeword: ");
-//    data_show(coded_word);
-//
-//    Poly decoded = poly_decode(coded_word);
-//    printf("Decoded word: ");
-//    data_show(decoded);
-//
-//    data_free(decoded);
-//    data_free(coded_word);
-//    data_free(message);
-//
-//
+
+    printf("Message: ");
+    Poly message = data_generate(K);
+    data_set(2, 1, message);
+    data_show(message);
+
+    printf("Coded word: ");
+    Poly coded_word = poly_encode(message);
+    data_show(coded_word);
+    data_set(6,1,coded_word); // Add an error
+    printf("Modified codeword: ");
+    data_show(coded_word);
+
+    Poly decoded = poly_decode(coded_word);
+    printf("Decoded word: ");
+    data_show(decoded);
+
+    data_free(decoded);
+    data_free(coded_word);
+    data_free(message);
+
+
     uint16_t i = 0;
     for(i=0; i<=N; i++)
         data_free(syndrome[i]);
 
     free(syndrome);
     data_free(generator);
-    //test();
-    }
+
     return EXIT_SUCCESS;
 }
 
